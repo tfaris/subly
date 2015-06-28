e08e31ff0f9a2a67d34ca089a94339953088867e
tfaris <ta.faris@gmail.com>
2015-06-27 21:49:02 -0400

    Pull subscription videos from upload playlists.
    Add video.py which defines classes, methods for requesting and
    interacting with youtube videos.

--- src/apps/subly/tasks.py
+++ src/apps/subly/tasks.py
@@ -7,6 +7,7 @@ from oauth2client.client import AccessTokenRefreshError
 from googleapiclient.errors import HttpError
 
 from auth import YTAuth
+from video import UploadPlaylistsVideoExtractor
 import message
 import exceptions
 
@@ -17,29 +18,19 @@ logger = logging.getLogger(__name__)
 
 class UpdatePlaylistsTask(object):
     def run(self, *args, **kwargs):
-
         auth = YTAuth()
+        videos = UploadPlaylistsVideoExtractor(auth)
         for user in self.users_to_update():
             try:
-                youtube = auth.get_service(user)
-                try:
-                    # Gather a list of all IDs of the channels that the user is subscribed to.
-                    sub_ids = []
-                    request = youtube.subscriptions().list(part='snippet', mine=True, maxResults=50)
-                    while request is not None:
-                        subs = request.execute()
-                        for item in subs.get('items', []):
-                            sub_ids.append(item['snippet']['resourceId']['channelId'])
-                        request = youtube.subscriptions().list_next(request, subs)
-                    # TODO: Get video list using sub_ids. Not sure which API yet. Don't want to have to query for
-                    # TODO: each subscription as that becomes costly and slow very quickly.
-                except AccessTokenRefreshError:
-                    message.add_message(user,
-                                        _("YouTube credentials have expired or been revoked. Please re-authorize"))
-                except HttpError as e:
-                    logger.exception(e)
-                    # TODO: Should probably check error code here and mark to retry on certain statuses (timeouts)
-                    raise e
+                user_videos = videos.get_videos(user)
+                # TODO: Apply users filters to videos.
+            except AccessTokenRefreshError:
+                message.add_message(user,
+                                    _("YouTube credentials have expired or been revoked. Please re-authorize"))
+            except HttpError as e:
+                logger.exception(e)
+                # TODO: Should probably check error code here and mark to retry on certain statuses (timeouts)
+                raise e
             except exceptions.InvalidCredentials:
                 message.add_message(user, _("Existing YouTube credentials are invalid. Please re-authorize."))
 
---
+++ src/apps/subly/video.py
@@ -0,0 +1,74 @@
+def chunks(l, n):
+    """
+    Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+
+class VideoExtractor(object):
+    def __init__(self, auth):
+        self._auth = auth
+
+    def get_videos(self, user):
+        """
+        Get videos for the specified user.
+        """
+        raise NotImplemented
+
+
+class UploadPlaylistsVideoExtractor(VideoExtractor):
+    """
+    Extract a YouTube users subscription videos using the method proposed by ali1234 here:
+    https://github.com/ali1234/ytsubs
+
+    1. Gets a list of user's subsciption IDs (using the subscription API).
+    2. Gets the ID of the upload playlist for each subscription (using the channels API).
+    3. Gets the X most recent video IDs in each upload playlist (using the playlistItems API), and
+    4. Gets details of each of the video IDs from step 3 (using the videos API).
+    """
+    def get_videos(self, user):
+        """
+        Get subscription videos for the specified user.
+        :param user:
+        :return:
+        """
+        youtube = self._auth.get_service(user)
+        # Gather a list of all IDs of the channels that the user is subscribed to.
+
+        request = youtube.subscriptions().list(part='snippet', mine=True, maxResults=50)
+        upload_playlists = []
+        while request is not None:
+            sub_ids = []
+            subs = request.execute()
+            for item in subs.get('items', []):
+                sub_ids.append(item['snippet']['resourceId']['channelId'])
+            # Get the ID of the uploads playlist for each subscription.
+            channels_request = youtube.channels().list(part='contentDetails', id=','.join(sub_ids), maxResults=50)
+            for item in channels_request.execute().get('items', []):
+                upload_playlists.append(item['contentDetails']['relatedPlaylists']['uploads'])
+            # Cycle to the next page of subs
+            request = youtube.subscriptions().list_next(request, subs)
+        all_upload_items = []
+        for playlist in upload_playlists:
+            all_upload_items.extend(self.get_playlist_items(youtube, playlist))
+        videos = []
+        for chunk in chunks(all_upload_items, 50):
+            videos.extend(self.get_video_info(youtube, chunk))
+        videos.sort(key=lambda i: i['snippet']['publishedAt'])
+        print videos
+
+    def get_playlist_items(self, youtube, playlist_id):
+        """
+        Get the video ids of videos in the specified playlist.
+        """
+        vids = []
+        pl_request = youtube.playlistItems().list(part='contentDetails', playlistId=playlist_id, maxResults=10)
+        for item in pl_request.execute().get('items', []):
+            vids.append(item['contentDetails']['videoId'])
+        return vids
+
+    def get_video_info(self, youtube, video_ids):
+        vids = []
+        vid_request = youtube.videos().list(part='snippet', id=','.join(video_ids), maxResults=50)
+        return vid_request.execute().get('items', [])
