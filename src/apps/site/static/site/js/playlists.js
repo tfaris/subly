2b74f69987dde87bed089db56b7265296adc8507
tfaris <ta.faris@gmail.com>
2015-10-17 01:23:25 -0400

    Submit playlist creation form when enter key is pressed.

--- src/apps/site/static/site/js/playlists.js
+++ src/apps/site/static/site/js/playlists.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
         var modal = $(this);
         modal.find('input[name="playlist_name"]').focus();
     });
+    // Submit the playlist creation form on enter key press.
+    $('#id_playlist_name').keypress(function (e) {
+        var key = e.which;
+        if(key == 13)  // Enter key
+        {
+            $(this).parents("form").first().submit()
+        }
+    });
 });
